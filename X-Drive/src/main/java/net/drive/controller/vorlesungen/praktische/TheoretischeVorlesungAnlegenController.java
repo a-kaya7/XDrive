ded481b42d2c5dbbe4cb854a32adb27084929f47
ahmet <ahmet@gmail.com>
2025-05-26 22:11:40 +0200

    PraktischeVorlesung Implementieren

--- X-Drive/src/main/java/net/drive/controller/vorlesungen/praktische/TheoretischeVorlesungAnlegenController.java
+++ X-Drive/src/main/java/net/drive/controller/vorlesungen/praktische/TheoretischeVorlesungAnlegenController.java
@@ -1,12 +1,15 @@
 package net.drive.controller.vorlesungen.praktische;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.drive.entities.vorlesung.theoretische.TheoretischeVorlesung;
 import net.drive.services.vorlesung.theoretische.aussensicht.ITheoretischeVorlesungAnlegen;
 
 @RestController
-@RequestMapping
+@RequestMapping("/vorlesung/anlegen")
 public class TheoretischeVorlesungAnlegenController {
 	
 	private final ITheoretischeVorlesungAnlegen theoretischeVorlesungAnlegen;
@@ -15,5 +18,8 @@ public class TheoretischeVorlesungAnlegenController {
 		this.theoretischeVorlesungAnlegen = theoretischeVorlesungAnlegen;
 	}
 
-	
+	@PostMapping
+	public TheoretischeVorlesung vorlesungAnlegen(@RequestBody TheoretischeVorlesung theoretischeVorlesung) {
+		return theoretischeVorlesungAnlegen.vorlesungAnlegen(theoretischeVorlesung);
+	}
 }
--- X-Drive/src/main/java/net/drive/services/vorlesung/theoretische/aussensicht/ITheoretischeVorlesungAnlegen.java
+++ X-Drive/src/main/java/net/drive/services/vorlesung/theoretische/aussensicht/ITheoretischeVorlesungAnlegen.java
@@ -1,5 +1,10 @@
 package net.drive.services.vorlesung.theoretische.aussensicht;
 
+import net.drive.entities.vorlesung.theoretische.TheoretischeVorlesung;
+
 public interface ITheoretischeVorlesungAnlegen {
 
+	
+
+	TheoretischeVorlesung vorlesungAnlegen(TheoretischeVorlesung theoretischeVorlesung, Long lehrerId);
 }
